8f5b8628c0b3e0bae290a5ff3dfbb7237b9b5f8a
Kelvin Ricafort <kelvs.r@outlook.com>
2023-09-26 15:22:45 +0800

    Networking in SwiftUI

---
+++ H4X0R News/Models/NetworkManager.swift
@@ -0,0 +1,29 @@
+//
+//  NetworkManager.swift
+//  H4X0R News
+//
+//  Created by Kelvin Ricafort on 9/26/23.
+//
+
+import Foundation
+
+class NetworkManager {
+    func fetchData() {
+        if let url = URL(string: "https://hn.algolia.com/api/v1/search?tags=front_page") {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error == nil {
+                    let decoder = JSONDecoder()
+                    if let safeData = data {
+                        do {
+                            let results = try decoder.decode(Results.self, from: safeData)
+                        } catch {
+                            print(error.localizedDescription)
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
+}
---
+++ H4X0R News/Models/PostData.swift
@@ -0,0 +1,22 @@
+//
+//  PostData.swift
+//  H4X0R News
+//
+//  Created by Kelvin Ricafort on 9/26/23.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String
+}
